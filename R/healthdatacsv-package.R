7b3f62179896c16fe62bd4de903adddde0694692
Ivan Castro <ivedcastr@gmail.com>
2019-11-11 22:06:34 +0000

    fix gp imports

--- R/healthdatacsv-package.R
+++ R/healthdatacsv-package.R
@@ -3,8 +3,8 @@
 #' @author Ivan Castro
 #' @name healthdatacsv
 #'
-#' @import dplyr
-#' @import httr
+#' @importFrom dplyr as_tibble select mutate filter
+#' @importFrom httr user_agent modify_url GET http_error
 #' @importFrom jsonlite fromJSON flatten
 #' @importFrom stringr str_detect
 #' @importFrom purrr map map_lgl
